1fd6e3d49d970451c51a2272eb8f04bb28d35471
Angelo Aroni <luis.angelo.0326@gmail.com>
2023-05-10 22:39:54 -0500

    mi primer commit

---
+++ assets/js/components/loader.js
@@ -0,0 +1,6 @@
+function loader(){
+    window.addEventListener('load', function(){
+        const loader = document.querySelector('.loader')
+        loader.classList.add('loader_hidden')
+    })
+}
